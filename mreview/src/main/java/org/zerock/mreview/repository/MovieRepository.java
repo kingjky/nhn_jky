c14d1e499efbf49b19b0142a682a7f6a2814f72a
Jeon Kyung Yoon <kyungyoon.jeon@nhn.com>
2021-01-22 11:17:11 +0900

    Add Movie Review

--- mreview/src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ mreview/src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -17,18 +17,22 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 //            + "left outer join MovieImage mi on mi.movie = m "
 //            + "left outer join Review r on r.movie = m group by m")
 
-    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
-            + " left outer join MovieImage mi on mi.movie = m "
-            + " left outer join Review r on r.movie = m group by m")
-
+    // max를 통한 N+1 테스트
 //    @Query("select m, max(r) from Movie m "
 //            + "left outer join Review r on r.movie = m group by m")
+
+//    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
+//            + " left outer join MovieImage mi on mi.movie = m "
+//            + " left outer join Review r on r.movie = m group by m")
+
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
+            + "left outer join MovieImage mi on mi.movie = m "
+            + "left outer join Review r on r.movie = m group by m ")
     Page<Object[]> getListPage(Pageable pageable);  // 페이지 처리
 
-    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(r) from Movie m "
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m "
         + " left outer join MovieImage mi on mi.movie = m "
         + " left outer join Review r on r.movie = m "
         + " where m.mno = :mno group by mi")
     List<Object[]> getMovieWithAll(Long mno);   //특정 영화 조회
-
 }
